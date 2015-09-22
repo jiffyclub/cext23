6b6a6ecffcdcbb1447957b8d8eadb82c57e68619
Matt Davis <jiffyclub@gmail.com>
2015-09-22 12:41:02 -0700

    add some comments to the setup.py file

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from Cython.Build import cythonize
 from setuptools import setup, find_packages, Extension
 
 
+# see args descriptions at
+# https://docs.python.org/3/distutils/apiref.html#distutils.core.Extension
 extensions = [
-    # see args descriptions at
-    # https://docs.python.org/3/distutils/apiref.html#distutils.core.Extension
+    # this compiles the code for the ctypes example
     Extension(
         name='cext23.ctypes._cext',
         sources=['src/add.c'],
         include_dirs=['src/']),
+    # this compiles the Cython example
     Extension(
         name='cext23.cython._cext',
         sources=['cext23/cython/_cext.pyx', 'src/add.c'],
