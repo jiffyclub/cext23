9a48813411506d7767e245a7f5a65dbe6783eac7
Matt Davis <jiffyclub@gmail.com>
2015-09-22 12:39:05 -0700

    add comment on cpdef to cython file

--- cext23/cython/_cext.pyx
+++ cext23/cython/_cext.pyx
@@ -1,4 +1,7 @@
 # See Cython docs at http://docs.cython.org/
 
 cdef extern from "add.h":
+    # the cpdef thing both declares the function, wraps it for Python,
+    # and includes it as part of the module, see
+    # http://docs.cython.org/src/tutorial/external.html#external-declarations
     cpdef int add(int a, int b)
