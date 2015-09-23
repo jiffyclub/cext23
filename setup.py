c98c054f150b43d9a0c945a5dddf48b2dcfb9c50
Matt Davis <jiffyclub@gmail.com>
2015-09-23 13:13:44 -0700

    add python setup.py test command

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
+import sys
+
 from Cython.Build import cythonize
 from setuptools import setup, find_packages, Extension
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args or '')
+        sys.exit(errno)
 
 
 # see args descriptions at
@@ -23,4 +45,6 @@ setup(
     ext_modules=cythonize(extensions),
     setup_requires=['cffi >= 1.1'],
     cffi_modules=['cext23/cffi/cextcffi_build.py:ffi'],
-    install_requires=['cffi >= 1.1'])
+    install_requires=['cffi >= 1.1', 'cython >= 0.23'],
+    tests_require=['pytest >= 2.7.3'],
+    cmdclass={'test': PyTest})
