4e62e314c956d1ab2daeeb899169b42b995808e9
Matt Davis <jiffyclub@gmail.com>
2015-09-25 10:40:52 -0700

    add demo function for adding two int32 arrays

--- src/demo.c
+++ src/demo.c
@@ -1,7 +1,17 @@
 #include "demo.h"
+#include <stdint.h>
 
 int scalar_int_add(int a, int b) {
     int c;
     c = a + b;
     return c;
+}
+
+
+int np_int32_add(int32_t* a, int32_t* b, int32_t* out, int size) {
+    for (int i = 0; i < size; ++i) {
+        out[i] = a[i] + b[i];
+    }
+
+    return 0;
 }
--- src/demo.h
+++ src/demo.h
@@ -1,1 +1,4 @@
+#include <stdint.h>
+
 int scalar_int_add(int a, int b);
+int np_int32_add(int32_t* a, int32_t* b, int32_t* out, int size);
